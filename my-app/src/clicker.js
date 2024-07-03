1ee4982e3705286de1ff2ad4443e940981e88e45
kirikalachev <kalachev.k.2507@gmail.com>
2024-07-03 15:07:50 +0300

    update 2

---
+++ my-app/src/clicker.js
@@ -0,0 +1,18 @@
+function Clicker() {
+  return (
+    <div className="clicker">
+      <div className="coinsCounter">
+
+      </div>
+
+      <div className='clickerButton'>
+
+      </div>
+    </div>
+  );
+}
+
+export default Clicker;
+
+/* <div className="coinsLeft"></div>
+<div className="boost"></div> */
---
+++ my-app/src/stats.js
@@ -0,0 +1,29 @@
+// import
+
+function Stats() {
+  return (
+    <div className="stats">
+        <div className="profile-stats">
+            <p className="name" contentEditable>Input name</p>
+        </div>
+
+        <div className="wrapper">
+            <div className="level">
+
+            </div>
+
+            <div className="child-wrapper">
+                <div className="profit">
+
+                </div>
+
+                <div className="settings">
+
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default Stats;
